b5e06437215145f508dca5c0c59a785f731b087f
caua-luz <121311881+caua-luz@users.noreply.github.com>
2025-06-09 20:21:59 -0300

    Update Materia.py

--- Materia.py
+++ Materia.py
@@ -11,7 +11,7 @@ fund_mecflu = materia("Fundamentos de Mecanica dos Fluidos", 3, 60)
 fund_termo = materia("Fundamentos de Termodinâmica", 3, 60)
 fund_mecso = materia("Fundamentos de Mecanica dos Solidos", 3, 60)
 fund_mecflu_termo = materia("Fundamentos de Mecanica dos Fluidos e Termodinâmica", 3, 60)
-fund_ooo = materia("Fundamentos de Ondas, Oscilações e Óptica", 4, 60)
+
 
 # ------------------------ Cálculos ------------------------
 c1 = materia("Calculo 1", 5, 90)
@@ -25,11 +25,7 @@ pds1 = materia("Programacao e Desenvolvimento de Software 1", 3, 60)
 pds2 = materia("Programacao e Desenvolvimento de Software 2", 3, 60)
 prog_comp = materia("Programacao de Computadores", 2, 60)
 
-Materias_Aeroespacial={c1,c2,c3,eda,fund_elmg,fund_mec,fund_mecflu,fund_mecso,prog_comp}
+Materias_Aeroespacial_Civil_Mecanica={c1,c2,c3,eda,fund_elmg,fund_mec,fund_mecflu,fund_termo,fund_mecso,prog_comp}
 
-Materias_Civil={c1,c2,c3,eda,fund_elmg,fund_mec,fund_mecflu,prog_comp}
-
-Materias_Mecanica={c1,c2,c3,eda,fund_elmg,fund_mec,fund_mecflu,fund_mecso,prog_comp}
-
-#Diferencial fooo e pds1 e pds2
-Materias_Eletrica={c1,c2,c3,eda,pds1,pds2,fund_elmg,fund_mec,fund_mecflu_termo,fund_ooo}
+#Diferencial e pds1 e pds2
+Materias_Eletrica={c1,c2,c3,eda,pds1,pds2,fund_elmg,fund_mec,fund_mecflu_termo}
